1ba924ca0c19197f22994c9fc69c02cf996332aa
sylic <2329766602@qq.com>
2025-02-18 21:41:56 +0800

    feat:11

--- components.d.ts
+++ components.d.ts
@@ -7,8 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElUpload: typeof import('element-plus/es')['ElUpload']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,17 @@
 import { createRouter,type RouteRecordRaw,createWebHistory } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/compressPhoto/:id',
+    path: '/compressPhoto',
     component: () => import("./views/compressImg/index.vue"),
+    props: true,
+    beforeEnter: (to, from) => {
+      // console.log("⭐ ~ 当前打印的内容 ~ :",to,from );  
+      return true;
+    },
+  },
+    {
+    path: '/extractText',
+    component: () => import("./views/ExtractText/index.vue"),
     props: true,
     beforeEnter: (to, from) => {
       // console.log("⭐ ~ 当前打印的内容 ~ :",to,from );  
